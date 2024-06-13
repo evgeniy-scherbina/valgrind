ca422e8f639e84bf517c559a58d32748ba16b078
evgeniy-scherbina <evgeniy.shcherbina.es@gmail.com>
2024-06-13 12:49:55 -0400

    Commit

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
-CGO_CFLAGS="-I/opt/homebrew/opt/rocksdb/include" \
-CGO_LDFLAGS="-L/opt/homebrew/opt/rocksdb/lib -luuid -lrocksdb -lstdc++ -lm -lz -L/opt/homebrew/opt/snappy/lib -L/opt/homebrew/opt/lz4/lib -L/opt/homebrew/opt/zstd/lib" \
+CGO_LDFLAGS="-luuid" \
 GOARCH=arm64 \
 CGO_ENABLED=1 \
   go install -v
